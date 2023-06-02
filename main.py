a095120cc2622d97b3c8357e0211af5dd1689992
Kotha Mukesh Raj <134232672+KothaMukeshRaj@users.noreply.github.com>
2023-06-03 01:29:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,19 +58,9 @@ decimal_button.grid(row=4, column=3)
 
 # Run the GUI
 window.mainloop()
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hi, {name}') 
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
